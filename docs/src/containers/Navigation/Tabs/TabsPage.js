fd8f5c2aa3923cb3227d60ffdb9f306dfc3b395b
David Martinez de Silva <dmtzds@gmail.com>
2021-04-05 12:42:40 -0500

    interactables scale animation

--- docs/src/containers/Navigation/Tabs/TabsPage.js
+++ docs/src/containers/Navigation/Tabs/TabsPage.js
@@ -101,12 +101,12 @@ export const TabsPage = (props) => {
         Tab 6
       </TabPanel>
 
-      <h2>Tabs with Label Prop</h2>
+      <h2>Tabs with Label and Icon Props</h2>
       <Tabs stretch={true} activeTab={activeTab}>
-        <Tab label="Home" onClick={() => setActiveTab(1)} tabId={1} />
-        <Tab label="Collections" onClick={() => setActiveTab(2)} tabId={2} />
-        <Tab label="Main Menu" onClick={() => setActiveTab(3)} tabId={3} />
-        <Tab label="Balance" onClick={() => setActiveTab(4)} tabId={4} />
+        <Tab icon="home" label="Home" onClick={() => setActiveTab(1)} tabId={1} />
+        <Tab icon="collection" label="Collections" onClick={() => setActiveTab(2)} tabId={2} />
+        <Tab icon="menu" label="Main Menu" onClick={() => setActiveTab(3)} tabId={3} />
+        <Tab icon="scale" label="Balance" onClick={() => setActiveTab(4)} tabId={4} />
         <Tab label="Review" onClick={() => setActiveTab(5)} tabId={5} />
         <Tab label="Checkout" onClick={() => setActiveTab(6)} tabId={6} />
       </Tabs>
--- src/components/Tabs/Tab/Tab.tsx
+++ src/components/Tabs/Tab/Tab.tsx
@@ -1,5 +1,11 @@
-import React, { ButtonHTMLAttributes, FunctionComponent, ReactNode, useState } from 'react';
-import classes from './Tab.module.css';
+import React, {
+  ButtonHTMLAttributes,
+  FunctionComponent,
+  ReactNode,
+  useState,
+} from "react";
+import { SvgIcon } from "../../SvgIcon";
+import classes from "./Tab.module.css";
 
 interface TabPropTypes {
   activeTab?: number;
@@ -9,9 +15,12 @@ interface TabPropTypes {
   tabId: number;
   wrapperClassName?: string;
   buttonClassName?: string;
-};
+  icon?: string;
+}
 
-export const Tab:FunctionComponent<TabPropTypes & ButtonHTMLAttributes<HTMLButtonElement>> = ({
+export const Tab: FunctionComponent<
+  TabPropTypes & ButtonHTMLAttributes<HTMLButtonElement>
+> = ({
   activeTab,
   children,
   label,
@@ -19,34 +28,44 @@ export const Tab:FunctionComponent<TabPropTypes & ButtonHTMLAttributes<HTMLButto
   tabId,
   wrapperClassName,
   buttonClassName,
+  icon,
   ...rest
 }) => {
-
-  let isActive = activeTab === tabId
+  let isActive = activeTab === tabId;
 
   let classList = [
-    classes['Tab'],
-    isActive ? classes['Tab--isActive'] : null,
-    wrapperClassName
+    classes["Tab"],
+    isActive ? classes["Tab--isActive"] : null,
+    wrapperClassName,
   ];
 
-  let buttonClassList = [
-    classes['TabButton'],
-    buttonClassName
-  ];
+  let buttonClassList = [classes["TabButton"], buttonClassName];
+
+  let labelInstance: ReactNode = label ? (
+    <p className={classes["tabLabel"]}>{label}</p>
+  ) : null;
+
+  let iconInstance: ReactNode = icon ? (
+    <SvgIcon
+      style={{marginRight: 6}}
+      size="extraSmall"
+      icon={icon}
+      color={isActive ? "primary" : null}
+    />
+  ) : null;
+
+  let contentInstance = (
+    <div className={classes['tabLabelContainer']}>
+      {iconInstance}
+      <span>{labelInstance}</span>
+    </div>
+  )
 
   return (
-    <div
-      id={tabId.toString()}
-      className={classList.join(' ')}
-    >
-      <button
-        onClick={onClick}
-        className={buttonClassList.join(' ')}
-        {...rest}
-      >
-        {children ? children : label}
+    <div id={tabId.toString()} className={classList.join(" ")}>
+      <button onClick={onClick} className={buttonClassList.join(" ")} {...rest}>
+        {children ? children : contentInstance}
       </button>
     </div>
   );
-}
+};
