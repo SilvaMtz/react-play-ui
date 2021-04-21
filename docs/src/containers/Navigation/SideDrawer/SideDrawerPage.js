20ca1d48bf8ef6d1a2f2b1a9a7b92ec333df0096
David Martinez de Silva <dmtzds@gmail.com>
2021-04-20 20:14:17 -0500

    Accordion

--- docs/src/containers/Navigation/SideDrawer/SideDrawerPage.js
+++ docs/src/containers/Navigation/SideDrawer/SideDrawerPage.js
@@ -1,9 +1,35 @@
 import { useState } from "react";
-import { ActionButton, SideDrawer } from "react-play-ui";
+import { Accordion, ActionButton, ContextMenu, SideDrawer } from "react-play-ui";
 
 export const SideDrawerPage = (props) => {
   const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
   const [isDocked, setIsDocked] = useState(false);
+
+  const layoutPanels = [
+    {
+      id: 1,
+      items: [
+        {
+          id: 1,
+          name: "Flex Grid",
+          label: "Flex Grid",
+          onClick: () => {}
+        },
+        {
+          id: 2,
+          name: "Flex Item",
+          label: "Flex Item",
+          onClick: () => {}
+        },
+        {
+          id: 3,
+          name: "Flex Group",
+          label: "Flex Group",
+          onClick: () => {}
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
@@ -14,7 +40,7 @@ export const SideDrawerPage = (props) => {
         isDocked={isDocked}
         toolbarZIndexLocation="below"
         isOpen={sideDrawerOpen}
-        style={{ padding: 24 }}
+        style={{ padding: 6 }}
         button={
           <ActionButton
             label="Open Drawer"
@@ -24,7 +50,26 @@ export const SideDrawerPage = (props) => {
           />
         }
       >
-        <ActionButton color="primary" fill onClick={() => setIsDocked(!isDocked)}>Docked: {isDocked ? "ON" : "OFF"}</ActionButton>
+        <ActionButton
+          restrainWidth={false}
+          color="primary"
+          fill
+          onClick={() => setIsDocked(!isDocked)}
+          style={{ marginBottom: 4 }}
+        >
+          Docked: {isDocked ? "ON" : "OFF"}
+        </ActionButton>
+        <Accordion
+          buttonPaddingSize="s"
+          paddingSize="none"
+          arrowDisplay="left"
+          icon="viewGrid"
+          iconSize="small"
+          buttonContent="Layout"
+          style={{ marginBottom: 4 }}
+        >
+          <ContextMenu transparent itemsSize="small" panels={layoutPanels} initialPanelId={1} />
+        </Accordion>
       </SideDrawer>
     </div>
   );
