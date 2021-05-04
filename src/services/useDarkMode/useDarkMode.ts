6ecac3834885bbffe72a88936795e1d517d18444
David Martinez de Silva <dmtzds@gmail.com>
2021-05-04 18:49:44 -0500

    persist dark mode

--- src/services/useDarkMode/useDarkMode.ts
+++ src/services/useDarkMode/useDarkMode.ts
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import { singletonHook } from 'react-singleton-hook';
 
-const initDarkMode = "light";
+const initDarkMode = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 let globalSetMode = (mode: string) => { throw new Error('you must useDarkMode before setting its state'); };
 
 export const useDarkMode = singletonHook(initDarkMode, () => {
   const [mode, setMode] = useState(initDarkMode);
-  globalSetMode = setMode as () => never;
+
+  const setNewMode = (newMode) => {
+    setMode(newMode);
+    localStorage.setItem('theme', newMode);
+  }
+
+  globalSetMode = setNewMode as () => never;
   return mode;
 });
 
